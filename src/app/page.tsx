7bc6ac8e55eafdb9d3c25edc441f8d6f60946b0d
ngtankhoa <40681049+ngtankhoa@users.noreply.github.com>
2024-09-28 18:02:29 +0700

    stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function HomePage() {
   const mockUrls = [
     "https://utfs.io/f/LQYonWKymt9sKB4WKd5PESNuZyGWe4aloX0s7fLirDncbY21",
     "https://utfs.io/f/LQYonWKymt9soamjlDAwYeFGsfZmIN4VaRcg5CO9B3DMWvS7",
-    "https://utfs.io/f/LQYonWKymt9sNfR2C0BXa3O2FV41Weinw9pgxCLPMBUTsHjR",
     "https://utfs.io/f/LQYonWKymt9sZqAeDDtahDFsjEkdACtgN9VO1zJHYB5K7qTX",
     "https://utfs.io/f/LQYonWKymt9sc9tz0B5Z5VOUYGljfmqbaHJTgFiQW0I97Aut",
     "https://utfs.io/f/LQYonWKymt9sERIPA7WypHjFLETkoCgQKmIzB0W5n9tbd8q6",
