5320d528b319a468b017012c7820c1b6cba5799d
ngtankhoa <40681049+ngtankhoa@users.noreply.github.com>
2024-09-29 11:57:57 +0700

    break query into separate file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,29 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+async function Images() {
+  const images = await getMyImages();
   console.log(images);
+  return images.map((item) => (
+    <div key={item.id} className="flex flex-col">
+      <img src={item.url} alt={item.name} />
+      {/* <Image src={}/> */}
+      <div>{item.name}</div>
+    </div>
+  ));
+}
 
+export default async function HomePage() {
   return (
     <main>
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4">
         <SignedOut>
           <p className="text-center text-xl">Please sign in above</p>
         </SignedOut>
         <SignedIn>
-          {images.map((item) => (
-            <div key={item.id} className="flex flex-col">
-              <img src={item.url} alt={item.name} />
-              <div>{item.name}</div>
-            </div>
-          ))}
+          <Images />
         </SignedIn>
       </div>
     </main>
