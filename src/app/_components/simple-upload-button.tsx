ae7c9e527de553f93e1a1acf861f2184d312e344
ngtankhoa <40681049+ngtankhoa@users.noreply.github.com>
2024-09-29 16:55:15 +0700

    sonner

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -64,7 +64,7 @@ function LoadingSpinnerSVG() {
       />
       <path
         d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
-        className="spinner_ajPY"
+        className="spinner_P7sC"
       />
     </svg>
   );
@@ -75,23 +75,23 @@ export function SimpleUploadButton() {
 
   const { inputProps } = useUploadThingInputProps("imageUploader", {
     onUploadBegin() {
-      //   toast(
-      //     <div className="flex items-center gap-2 text-white">
-      //       <LoadingSpinnerSVG /> <span className="text-lg">Uploading...</span>
-      //     </div>,
-      //     {
-      //       duration: 100000,
-      //       id: "upload-begin",
-      //     },
-      //   );
+      toast(
+        <div className="flex items-center gap-2 text-white">
+          <LoadingSpinnerSVG /> <span className="text-lg">Uploading...</span>
+        </div>,
+        {
+          duration: 100000,
+          id: "upload-begin",
+        },
+      );
     },
     onUploadError(error) {
-      //   toast.dismiss("upload-begin");
-      //   toast.error("Upload failed");
+      toast.dismiss("upload-begin");
+      toast.error("Upload failed");
     },
     onClientUploadComplete() {
-      //   toast.dismiss("upload-begin");
-      //   toast("Upload complete!");
+      toast.dismiss("upload-begin");
+      toast("Upload complete!");
 
       router.refresh();
     },
